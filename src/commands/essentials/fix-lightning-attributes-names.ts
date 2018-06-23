544c436ce60050f1adbf9882a8cd22ff562ac7bd
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2018-06-23 02:40:21 +0200

    Progressing ...

--- src/commands/essentials/fix-lightning-attributes-names.ts
+++ src/commands/essentials/fix-lightning-attributes-names.ts
@@ -12,9 +12,14 @@ export default class ExecuteFilter extends Command {
   }
 
   static args = []
+
   // global variables
   reservedAttributeNames = {}
+  reservedAttributeNamesUsed = {}
   folder = '.'
+  totalReplacements = 0
+
+
 
   // Internal properties
   fs = require('fs')
@@ -40,9 +45,34 @@ export default class ExecuteFilter extends Command {
     customApexClassFileNameList.forEach(customApexClassFileName => {
       customApexClassList.push(this.cint.after(customApexClassFileName, '/classes/').replace('.cls', ''))
     })
-    console.log('Custom apex class names :' + JSON.stringify(customApexClassList))
-    Object.keys(customApexClassList).forEach(customApexClass => {
-      this.reservedAttributeNames[customApexClass] = { type: 'apexClass' }
+    console.log('Custom apex class names :' + JSON.stringify(customApexClassList, null, 2))
+    customApexClassList.forEach(customApexClass => {
+      this.reservedAttributeNames[customApexClass] = {
+        type: 'apexClass',
+        numberReplacements: 0,
+        numberReplacementsCmp: 0,
+        numberReplacementsJs: 0,
+        numberReplacementsApex: 0
+      }
+    })
+
+    // Add list of objects names in reserved variables
+    var fetchObjectsExpression = this.folder + '/objects/*'
+    console.log('Fetching objects with expression : ' + fetchObjectsExpression)
+    var objectsFolderNameList = this.glob.sync(fetchObjectsExpression)
+    var objectNameList = []
+    objectsFolderNameList.forEach(objectFolderName => {
+      objectNameList.push(this.cint.after(objectFolderName, '/objects/'))
+    })
+    console.log('objects names :' + JSON.stringify(objectNameList, null, 2))
+    objectNameList.forEach(objectName => {
+      this.reservedAttributeNames[objectName] = {
+        type: 'object',
+        numberReplacements: 0,
+        numberReplacementsCmp: 0,
+        numberReplacementsJs: 0,
+        numberReplacementsApex: 0
+      }
     })
 
     // Build replacement names for each reserved attribute name
@@ -51,88 +81,158 @@ export default class ExecuteFilter extends Command {
       var reservedAttributeNameReplacement = this.camelCase(reservedAttributeName)
       self.reservedAttributeNames[reservedAttributeName].replacement = reservedAttributeNameReplacement
     })
+    console.log('Attributes replacements :\n' + JSON.stringify(self.reservedAttributeNames, null, 2))
 
-    // Process replacements
+    ////////// Process replacements
+
+    // Process replacements on components
     var fetchCmpExpression = this.folder + '/aura/*'
     console.log('Fetching components with expression : ' + fetchCmpExpression)
     var customComponentFolderList = this.glob.sync(fetchCmpExpression)
-    console.log('Component files: ' + JSON.stringify(customComponentFolderList))
+    console.log('Component files: ' + JSON.stringify(customComponentFolderList, null, 2))
+    var replacementPromisesCmp = []
     customComponentFolderList.forEach(customComponentFolder => {
-      self.processComponentXml(customComponentFolder).then(function() {
-
-      }).then(function() {
-        this.processComponentController(customComponentFolder)
-      } ).then(function() {
-        this.processComponentHelper(customComponentFolder)
-      })
+      replacementPromisesCmp.push(new Promise(function (resolve, reject) {
+        // Get file name
+        var tokenLs = customComponentFolder.split('/')
+        var filePart = tokenLs[tokenLs.length - 1]
+        // Replace in .cmp, .app & .evt files (send function as parameter)
+        self.processFile(customComponentFolder, filePart, ['.cmp', '.app', '.evt'], self.replaceAttributeNamesInCmp).then(function () {
+          // Replace in component controller (send function as parameter)
+          return self.processFile(customComponentFolder, filePart, ['Controller.js'], self.replaceAttributeNamesInJs)
+        }).then(function () {
+          // Replace in component helper javascript (send function as parameter)
+          return self.processFile(customComponentFolder, filePart, ['Helper.js'], self.replaceAttributeNamesInJs)
+        }).then(function () {
+          resolve()
+        }).catch(function (err) {
+          console.log('Replacement promise error: ' + err)
+          resolve()
+        })
+      }))
     })
 
+    Promise.all(replacementPromisesCmp).then(function () {
+      // Process replacements on apex classes
+      var replacementPromisesApex = []
+      Object.keys(self.reservedAttributeNames).forEach(reservedAttributeName => {
+        // Check if there has been replacements in lightning components
+        if (self.reservedAttributeNames[reservedAttributeName].type === 'apexClass') {
+          replacementPromisesApex.push(new Promise(function (resolve, reject) {
+            // Replace in apex class files (.cls) (send function as parameter)
+            self.processFile(self.folder + '/classes', reservedAttributeName, ['.cls'], self.replaceAttributeNamesInApex)
+              .then(function () {
+                console.log(' resolved cls: ' + reservedAttributeName)
+                resolve()
+              }).catch(function (err) {
+                console.log('Replacement promise error: ' + err)
+                reject()
+              })
+          }))
+        }
+      })
+
+      // Log summary
+      Promise.all(replacementPromisesApex).then(function () {
+        Object.keys(self.reservedAttributeNames).forEach(reservedAttributeName => {
+          if (self.reservedAttributeNames[reservedAttributeName].numberReplacements > 0) {
+            self.reservedAttributeNamesUsed[reservedAttributeName] = self.reservedAttributeNames[reservedAttributeName]
+          }
+        });
+        console.log('Attributes replacements results :\n' + JSON.stringify(self.reservedAttributeNamesUsed, null, 2))
+        console.log('Total replacements :' + self.totalReplacements)
+      }).catch(function (err) {
+        console.log('Promises error: ' + err)
+      })
+    })
   }
 
   // Process component file
-  processComponentXml(customComponentFolder) {
+  processFile(customComponentFolder, filePart, extensions, replaceFunction) {
     var self = this
     return new Promise(function (resolve, reject) {
-      // Define file name ( can be .cmp ,.app or .evt )
-      var tokenLs = customComponentFolder.split('/')
-      var filePath = customComponentFolder + '/' + tokenLs[tokenLs.length - 1] + '.cmp'
-      if (!self.fs.existsSync(filePath))
-        filePath = customComponentFolder + '/' + tokenLs[tokenLs.length - 1] + '.app'
-      if (!self.fs.existsSync(filePath)) {
-        filePath = customComponentFolder + '/' + tokenLs[tokenLs.length - 1] + '.evt'
-      }
-      if (!self.fs.existsSync(filePath)) {
-        console.warn('Warning: aura folder not taken in account: ' + customComponentFolder)
-      }
-
-      // Read file
-      var lineReader = require('readline').createInterface({
-        input: self.fs.createReadStream(filePath)
+      // Find file
+      var filePath = null
+      extensions.forEach(extension => {
+        var filePathTest = customComponentFolder + '/' + filePart + extension
+        if (filePath == null && self.fs.existsSync(filePathTest))
+          filePath = filePathTest
       });
-      console.log(JSON.stringify(lineReader, null, 2))
-      var updatedFileContent = ''
+      if (filePath == null) {
+        resolve()
+        return
+      }
+      // Read file line by line & process them
       var updated = false
-      lineReader.on('line', function (xmlLine) {
-        var newLine = self.replaceAttributeNames(xmlLine.substr(0)) // (clone xmlLine object to be able to compare later)
+      var updatedFileContent = ''
+      var readEachLineSync = require('read-each-line-sync');
+      readEachLineSync(filePath, function (line) {
+        var newLine = replaceFunction.call(self, line.substr(0)) // (clone line var to be able to compare later)
         updatedFileContent += newLine
-        console.log('Before: ' + xmlLine)
-        console.log('After : ' + newLine)
-        if (updated === false && newLine !== xmlLine)
+        if (updated === false && newLine !== line)
           updated = true
-      });
-      lineReader.on('end',function () {
-        // Close file reader
-        lineReader.close()
-        // Update file if it has been modified   
-        if (updated) {
-          self.fs.writeFileSync(filePath, updatedFileContent)
-          console.log('Updated ' + filePath + ' with content :\n' + updatedFileContent)
-          resolve() // Resolve promise so then is called
-        }
       })
+      if (updated) {
+        //self.fs.writeFileSync(filePath, updatedFileContent)
+        console.log('Updated ' + filePath)//+ ' with content :\n' + updatedFileContent)
+      }
+      resolve() // Resolve promise so then is called
     })
   }
 
-  replaceAttributeNames(xmlLine) {
+  // Replace attribute names in comonent ( xml )
+  replaceAttributeNamesInCmp(xmlLine) {
+    var self = this
     Object.keys(this.reservedAttributeNames).forEach(reservedAttributeName => {
       // Attribute name
-      xmlLine = xmlLine.replace(`name="${reservedAttributeName}"`, `name="${this.reservedAttributeNames[reservedAttributeName].replacement}"`)
-      xmlLine = xmlLine.replace(`name= "${reservedAttributeName}"`, `name="${this.reservedAttributeNames[reservedAttributeName].replacement}"`)
-      xmlLine = xmlLine.replace(`name ="${reservedAttributeName}"`, `name="${this.reservedAttributeNames[reservedAttributeName].replacement}"`)
-      xmlLine = xmlLine.replace(`name = "${reservedAttributeName}"`, `name="${this.reservedAttributeNames[reservedAttributeName].replacement}"`)
+      xmlLine = self.replaceExpression(xmlLine, reservedAttributeName, `name="${reservedAttributeName}"`, `name="${self.reservedAttributeNames[reservedAttributeName].replacement}"`, 'cmp')
+      xmlLine = self.replaceExpression(xmlLine, reservedAttributeName, `name= "${reservedAttributeName}"`, `name="${self.reservedAttributeNames[reservedAttributeName].replacement}"`, 'cmp')
+      xmlLine = self.replaceExpression(xmlLine, reservedAttributeName, `name ="${reservedAttributeName}"`, `name="${self.reservedAttributeNames[reservedAttributeName].replacement}"`, 'cmp')
+      xmlLine = self.replaceExpression(xmlLine, reservedAttributeName, `name = "${reservedAttributeName}"`, `name="${self.reservedAttributeNames[reservedAttributeName].replacement}"`, 'cmp')
     })
     return xmlLine
   }
 
-  processComponentController(xmlLine) {
-    return new Promise(function (resolve, reject) {
-      resolve()
+
+  // Replace attribute names in javascript file
+  replaceAttributeNamesInJs(jsLine) {
+    var self = this
+    Object.keys(this.reservedAttributeNames).forEach(reservedAttributeName => {
+      // Attribute name
+      jsLine = self.replaceExpression(jsLine, reservedAttributeName, `'v.${reservedAttributeName}'`, `'v.${self.reservedAttributeNames[reservedAttributeName].replacement}'`, 'js')
+      jsLine = self.replaceExpression(jsLine, reservedAttributeName, `"v.${reservedAttributeName}"`, `"v.${self.reservedAttributeNames[reservedAttributeName].replacement}"`, 'js')
+      jsLine = self.replaceExpression(jsLine, reservedAttributeName, `v.${reservedAttributeName}.`, `v.${self.reservedAttributeNames[reservedAttributeName].replacement}.`, 'js')
     })
+    return jsLine
   }
-  processComponentHelper(xmlLine) {
-    return new Promise(function (resolve, reject) {
-      resolve()
-    })    
+
+  // Replace attribute names in javascript file
+  replaceAttributeNamesInApex(apexLine) {
+    var self = this
+    Object.keys(this.reservedAttributeNames).forEach(reservedAttributeName => {
+      // Attribute name
+      apexLine = self.replaceExpression(apexLine, reservedAttributeName, `.get('${reservedAttributeName}'`, `.get('${self.reservedAttributeNames[reservedAttributeName].replacement}'`, 'apex')
+    })
+    return apexLine
+  }
+
+  // Replace expression in given line
+  replaceExpression(line, reservedAttributeName, expressionToReplace, replacement, type) {
+    if (line.includes(expressionToReplace)) {
+      console.log('- found: ' + expressionToReplace + ' in ' + line)
+      line = line.replace(expressionToReplace, replacement)
+      console.log('-- replaced by: ' + line)
+      this.reservedAttributeNames[reservedAttributeName].numberReplacements = this.reservedAttributeNames[reservedAttributeName].numberReplacements + 1
+      this.totalReplacements = this.totalReplacements + 1
+      if (type === 'cmp')
+        this.reservedAttributeNames[reservedAttributeName].numberReplacementsCmp = this.reservedAttributeNames[reservedAttributeName].numberReplacementsCmp + 1
+      else if (type === 'js')
+        this.reservedAttributeNames[reservedAttributeName].numberReplacementsJs = this.reservedAttributeNames[reservedAttributeName].numberReplacementsJs + 1
+      else if (type === 'apex')
+        this.reservedAttributeNames[reservedAttributeName].numberReplacementsApex = this.reservedAttributeNames[reservedAttributeName].numberReplacementsApex + 1
+
+    }
+    return line
   }
 
 }
