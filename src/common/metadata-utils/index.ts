0358f78902ab87113e69f492cb7d875539880421
Nikkii <ntang6@dxc.com>
2020-01-07 11:31:07 +0100

    Script for permission sets generation
    Example : 
    sfdx essentials:generate-permission-sets -c "../sfdx-essentials/src/commands/essentials/generate-permission-sets-config.json" -p "Config/packageXml/package_DevRoot_Managed.xml"

--- src/common/metadata-utils/index.ts
+++ src/common/metadata-utils/index.ts
@@ -7,21 +7,22 @@ export class MetadataUtils {
     // nameSuffixList are the files and/or folder names , built from the name of the package.xml item ( in <members> )
 
     const metadataTypesDescription = {
+
       // Metadatas to use for copy
-      ApexClass: { folder: 'classes', nameSuffixList: ['.cls', '.cls-meta.xml'], sfdxNameSuffixList: ['.cls', '-meta.xml'] },
+      ApexClass: { folder: 'classes', nameSuffixList: ['.cls', '.cls-meta.xml'], sfdxNameSuffixList: ['.cls', '-meta.xml'], permissionSetTypeName: 'classAccesses', permissionSetMemberName: 'apexClass' },
       ApexComponent: { folder: 'components', nameSuffixList: ['.component', '.component-meta.xml'], sfdxNameSuffixList: ['.component', '.component-meta.xml'] },
-      ApexPage: { folder: 'pages', nameSuffixList: ['.page', '.page-meta.xml'], sfdxNameSuffixList: ['.page', '-meta.xml'] },
+      ApexPage: { folder: 'pages', nameSuffixList: ['.page', '.page-meta.xml'], sfdxNameSuffixList: ['.page', '-meta.xml'], permissionSetTypeName: 'pageAccesses', permissionSetMemberName: 'apexPage' },
       ApexTrigger: { folder: 'triggers', nameSuffixList: ['.trigger', '.trigger-meta.xml'], sfdxNameSuffixList: ['.trigger', '-meta.xml'] },
       ApprovalProcess: { folder: 'approvalProcesses', nameSuffixList: ['.approvalProcess'], sfdxNameSuffixList: ['.approvalProcess-meta.xml'] },
       AuraDefinitionBundle: { folder: 'aura', nameSuffixList: [''], sfdxNameSuffixList: [''] },
       LightningComponentBundle: { folder: 'lwc', nameSuffixList: [''], sfdxNameSuffixList: [''] },
       ContentAsset: { folder: 'contentassets', nameSuffixList: ['.asset', '.asset-meta.xml'], sfdxNameSuffixList: ['.asset', '.asset-meta.xml'] },
-      CustomApplication: { folder: 'applications', nameSuffixList: ['.app'], sfdxNameSuffixList: ['.app-meta.xml'] },
+      CustomApplication: { folder: 'applications', nameSuffixList: ['.app'], sfdxNameSuffixList: ['.app-meta.xml'], permissionSetTypeName: 'applicationVisibilities', permissionSetMemberName: 'application' },
       CustomLabel: { folder: 'labels', nameSuffixList: ['.labels'], sfdxNameSuffixList: ['.labels-meta.xml'] },
       CustomMetadata: { folder: 'customMetadata', nameSuffixList: ['.md'], sfdxNameSuffixList: ['.md-meta.xml'] },
       CustomSite: { folder: 'sites', nameSuffixList: ['.site'], sfdxNameSuffixList: ['.site-meta.xml'] },
-      //      'CustomObjectTranslation': { folder: 'objectTranslations', nameSuffixList: ['.objectTranslation'] }, we use Translations to define the list of objectTranslations to filter & copy
-      CustomTab: { folder: 'tabs', nameSuffixList: ['.tab'], sfdxNameSuffixList: ['.tab-meta.xml'] },
+      CustomObjectTranslation: { folder: 'objectTranslations', nameSuffixList: ['.objectTranslation'] }, // We use Translations to define the list of objectTranslations to filter & copy
+      CustomTab: { folder: 'tabs', nameSuffixList: ['.tab'], sfdxNameSuffixList: ['.tab-meta.xml'], permissionSetTypeName: 'tabSettings', permissionSetMemberName: 'tab' },
       Document: { folder: 'documents', nameSuffixList: ['', '-meta.xml'], sfdxNameSuffixList: ['.documentFolder-meta.xml'] },
       EmailTemplate: { folder: 'email', nameSuffixList: ['.email', '.email-meta.xml'], sfdxNameSuffixList: [] },
       EscalationRules: { folder: 'escalationRules', nameSuffixList: ['.escalationRules'], sfdxNameSuffixList: ['.escalationRules-meta.xml'] },
@@ -52,10 +53,10 @@ export class MetadataUtils {
       // Metadatas to use for building objects folder ( SObjects )
       BusinessProcess: { sobjectRelated: true },
       CompactLayout: { sobjectRelated: true },
-      CustomField: { sobjectRelated: true },
-      CustomObject: { sobjectRelated: true },
+      CustomField: { sobjectRelated: true, permissionSetTypeName: 'fieldPermissions', permissionSetMemberName: 'field' },
+      CustomObject: { sobjectRelated: true, permissionSetTypeName: 'objectPermissions', permissionSetMemberName: 'object' },
       ListView: { sobjectRelated: true },
-      RecordType: { sobjectRelated: true },
+      RecordType: { sobjectRelated: true, permissionSetTypeName: 'recordTypeVisibilities', permissionSetMemberName: 'recordType' },
       WebLink: { sobjectRelated: true },
       ValidationRule: { sobjectRelated: true },
       FieldSet: { sobjectRelated: true },
@@ -64,6 +65,7 @@ export class MetadataUtils {
       Translations: { translationRelated: true, folder: 'translations', nameSuffixList: ['.translation'], sfdxNameSuffixList: ['.translation-meta.xml'] }
 
     };
+
     return metadataTypesDescription;
   }
 
@@ -110,7 +112,6 @@ export class MetadataUtils {
     ];
     return objectFilteringProperties;
   }
-
 }
 
 module.exports = MetadataUtils;
