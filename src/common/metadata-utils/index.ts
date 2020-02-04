664d9d021e2d99bc0cc968292f4a49a3381ba3d7
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2020-02-04 18:07:35 +0100

    v1.5.12 Fix filter-metadatas bug for NetworkBranding

--- src/common/metadata-utils/index.ts
+++ src/common/metadata-utils/index.ts
@@ -35,7 +35,7 @@ export class MetadataUtils {
       Layout: { folder: 'layouts', nameSuffixList: ['.layout'], sfdxNameSuffixList: ['.layout-meta.xml'] },
       NamedCredential: { folder: 'namedCredentials', nameSuffixList: ['.namedCredential'], sfdxNameSuffixList: ['.namedCredential-meta.xml'] },
       Network: { folder: 'networks', nameSuffixList: ['.network'], sfdxNameSuffixList: ['.network-meta.xml'] },
-      NetworkBranding: { folder: 'networkBranding', nameSuffixList: ['', '.networkBranding'], sfdxNameSuffixList: ['.networkBranding-meta.xml', '.networkBranding'] },
+      NetworkBranding: { folder: 'networkBranding', nameSuffixList: ['', '.networkBranding', '.networkBranding-meta.xml'], sfdxNameSuffixList: ['.networkBranding-meta.xml', '.networkBranding'] },
       PermissionSet: { folder: 'permissionsets', nameSuffixList: ['.permissionset'], sfdxNameSuffixList: ['.permissionset-meta.xml'] },
       PlatformCachePartition: { folder: 'cachePartitions', nameSuffixList: ['.cachePartition'], sfdxNameSuffixList: ['.cachePartition-meta.xml'] },
       Profile: { folder: 'profiles', nameSuffixList: ['.profile'], sfdxNameSuffixList: ['.profile-meta.xml'] },
