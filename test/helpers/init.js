574297ba3cd7cde909f38202bf2041235da04d53
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2020-02-10 13:26:03 +0100

    circle

--- test/helpers/init.js
+++ test/helpers/init.js
@@ -1,2 +1,7 @@
-const path = require('path')
-process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json')
+const path = require('path');
+const fs = require('fs');
+process.env.TS_NODE_PROJECT = path.resolve('test/tsconfig.json');
+if (!fs.existsSync('./test/tmp')) {
+    console.log('Created test/tmp folder');
+    fs.mkdirSync('./test/tmp');
+}
